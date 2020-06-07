5a17d0e175324fc5166f1eaab01f86c2496a0738
le123u <63946085+le123u@users.noreply.github.com>
2020-06-07 15:42:54 +0800

    接口的多继承

---
+++ src/com/neusoft/K.java
@@ -0,0 +1,14 @@
+package com.neusoft;
+
+/**
+ * @author Liu
+ * @date 2020/6/7 15:40
+ */
+public class K implements F,G {
+
+    // default 不保留
+    @Override
+    public void method() {
+
+    }
+}
